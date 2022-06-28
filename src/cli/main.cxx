4df1d899046dd42eae916b9822915245aa0a8426
Anastasios Koutian <akoutian2@gmail.com>
2022-06-28 21:20:00 +0100

    Make arguments actually faithful to original pass
    Using lyra::command. Also remove lyra's help messages from the cli,
    since we are providing our own 'help' command. Finally, make 'show' the
    default command.

--- src/cli/main.cxx
+++ src/cli/main.cxx
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "info/info.h"
+#include "show/show.h"
 
 #include <lyra/lyra.hpp>
 
@@ -12,14 +13,25 @@ namespace
 struct Args
 {
     bool help{};
+    bool show{};
     bool version{};
 };
 
 auto BuildCli(Args &args)
 {
     auto cli = lyra::cli();
-    cli |= lyra::opt(args.help)["-h"]["--help"]("Show this text.");
-    cli |= lyra::opt(args.version)["-v"]["--version"]("Show version information.");
+
+    const auto help = [&]([[maybe_unused]] const lyra::group &g) { args.help = true; };
+    cli |= lyra::opt(args.help).name("-h").name("--help");
+    cli |= lyra::command("help", help);
+
+    const auto show = [&]([[maybe_unused]] const lyra::group &g) { args.show = true; };
+    cli |= lyra::command("show", show);
+
+    const auto version = [&]([[maybe_unused]] const lyra::group &g) { args.version = true; };
+    cli |= lyra::opt(args.version).name("-v").name("--version");
+    cli |= lyra::command("version", version);
+
     return cli;
 }
 
@@ -42,11 +54,19 @@ int main(int argc, char **argv)
         return EXIT_SUCCESS;
     }
 
+    if (args.show)
+    {
+        pass::Show();
+        return EXIT_SUCCESS;
+    }
+
     if (args.version)
     {
         pass::Version();
         return EXIT_SUCCESS;
     }
+
+    pass::Show();
 
     return EXIT_SUCCESS;
 }
