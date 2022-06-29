a9724c2f7831383e357257adf71cfd0908322706
Anastasios Koutian <akoutian2@gmail.com>
2022-06-29 19:11:01 +0100

    Use auto in lambda args

--- src/cli/main.cxx
+++ src/cli/main.cxx
@@ -21,14 +21,14 @@ auto BuildCli(Args &args)
 {
     auto cli = lyra::cli();
 
-    const auto help = [&]([[maybe_unused]] const lyra::group &g) { args.help = true; };
+    const auto help = [&]([[maybe_unused]] const auto &a) { args.help = true; };
     cli |= lyra::opt(args.help).name("-h").name("--help");
     cli |= lyra::command("help", help);
 
-    const auto show = [&]([[maybe_unused]] const lyra::group &g) { args.show = true; };
+    const auto show = [&]([[maybe_unused]] const auto &a) { args.show = true; };
     cli |= lyra::command("show", show);
 
-    const auto version = [&]([[maybe_unused]] const lyra::group &g) { args.version = true; };
+    const auto version = [&]([[maybe_unused]] const auto &a) { args.version = true; };
     cli |= lyra::opt(args.version).name("-v").name("--version");
     cli |= lyra::command("version", version);
 
