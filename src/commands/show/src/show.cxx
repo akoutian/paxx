70fdd8c2b0f2ac0769f7e05508bde168f7ae40ce
Anastasios Koutian <akoutian2@gmail.com>
2022-07-02 15:34:13 +0100

    Prototype a function to find the password store

--- src/commands/show/src/show.cxx
+++ src/commands/show/src/show.cxx
@@ -2,14 +2,63 @@
 
 #include "show/show.h"
 
-#include <stdexcept>
+#include <filesystem>
+#include <iostream>
+#include <optional>
 
 namespace pass
 {
 
-void Show([[maybe_unused]] cmn::Info &info)
+namespace fs = std::filesystem;
+
+namespace
 {
-    throw std::runtime_error("TODO: implement");
+
+static std::string gl_passwordStoreName = "password-store";
+
+std::optional<fs::path> FindPasswordStore()
+{
+    fs::path result;
+
+    if (const auto h = std::getenv("HOME"))
+    {
+        const auto p = fs::path(h) /= "." + gl_passwordStoreName;
+        if (fs::exists(p))
+        {
+            result = p;
+        }
+    }
+
+    if (const auto h = std::getenv("PASSWORD_STORE_DIR"))
+    {
+        const auto p = fs::path(h);
+        if (fs::exists(p))
+        {
+            result = p;
+        }
+    }
+
+    return result.empty() ? std::nullopt : std::make_optional(result);
+}
+
+} // namespace
+
+void Show(cmn::Info &info)
+{
+    const auto p = FindPasswordStore();
+
+    if (p)
+    {
+        for (const auto &d : fs::recursive_directory_iterator(*p))
+        {
+            std::cout << d << std::endl;
+        }
+    }
+    else
+    {
+        info.status = 1;
+        info.message = "Error: password store is empty. Try \"pass init\".";
+    }
 }
 
 } // namespace pass
