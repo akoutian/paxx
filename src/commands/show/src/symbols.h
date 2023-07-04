82bdfb7136f70eada326ab2b763259bcf62ca5b7
Anastasios Koutian <akoutian2@gmail.com>
2023-07-04 21:24:58 +0100

    Put symbols in tree namespace

--- src/commands/show/src/symbols.h
+++ src/commands/show/src/symbols.h
@@ -4,15 +4,15 @@
 
 #include <cstddef>
 
-namespace pass
+namespace pass::tree
 {
 
+static constexpr auto gl_bar = "\342\224\202";
 static constexpr auto gl_corner = "\342\224\224";
 static constexpr auto gl_dash = "\342\224\200";
-static constexpr auto gl_bar = "\342\224\202";
 static constexpr auto gl_newline = '\n';
 static constexpr auto gl_space = ' ';
 static constexpr auto gl_tee = "\342\224\234";
 static constexpr size_t gl_indent_depth = 4;
 
-} // namespace pass
+} // namespace pass::tree
