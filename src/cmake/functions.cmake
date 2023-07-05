d2594c022a448527c5f25a822ec18b812061748e
Anastasios Koutian <akoutian2@gmail.com>
2023-07-05 08:15:42 +0100

    Change argument names in test function

--- src/cmake/functions.cmake
+++ src/cmake/functions.cmake
@@ -1,17 +1,17 @@
 # convenience function for creating tests
 function(add_pass_test)
   set(opts)
-  set(oneValueArgs BIN_NAME)
-  set(multiValueArgs BIN_SOURCES)
+  set(oneValueArgs NAME)
+  set(multiValueArgs SOURCES)
   cmake_parse_arguments(
     ADD_PASS_TEST "${opts}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN}
   )
-  add_executable(${ADD_PASS_TEST_BIN_NAME} ${ADD_PASS_TEST_BIN_SOURCES})
+  add_executable(${ADD_PASS_TEST_NAME} ${ADD_PASS_TEST_SOURCES})
   target_include_directories(
-    ${ADD_PASS_TEST_BIN_NAME} PRIVATE ${CMAKE_CURRENT_LIST_DIR}
+    ${ADD_PASS_TEST_NAME} PRIVATE ${CMAKE_CURRENT_LIST_DIR}
   )
   set(doctestArgs "-fc")
-  add_test(NAME ${ADD_PASS_TEST_BIN_NAME} COMMAND ${ADD_PASS_TEST_BIN_NAME}
-                                                  ${doctestArgs}
+  add_test(NAME ${ADD_PASS_TEST_NAME} COMMAND ${ADD_PASS_TEST_NAME}
+                                              ${doctestArgs}
   )
 endfunction()
