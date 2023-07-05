a59ae9ce9b7cfc1fa68e78d86b0185ddfbb3132b
Anastasios Koutian <akoutian2@gmail.com>
2023-07-05 07:47:46 +0100

    Change cmake-format parameters
    Increase line length to 100 characters
    Increase indendation depth to 4 spaces
    
    This is done to acheive a more uniform look with the rest of the code

--- src/cmake/conan-setup.cmake
+++ src/cmake/conan-setup.cmake
@@ -1,39 +1,33 @@
 cmake_minimum_required(VERSION 3.20)
 
 if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
-  message(
-    STATUS
-      "Downloading conan.cmake from https://github.com/conan-io/cmake-conan"
-  )
-  file(
-    DOWNLOAD
-    "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
-    "${CMAKE_BINARY_DIR}/conan.cmake" TLS_VERIFY ON
-  )
+    message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+         "${CMAKE_BINARY_DIR}/conan.cmake" TLS_VERIFY ON
+    )
 endif()
 
 include(${CMAKE_BINARY_DIR}/conan.cmake)
 
 if(BUILD_TESTING)
-  list(APPEND BUILD_REQUIRES doctest/2.4.8)
+    list(APPEND BUILD_REQUIRES doctest/2.4.8)
 endif()
 
 conan_cmake_configure(
-  REQUIRES lyra/1.6.0 BUILD_REQUIRES ${BUILD_REQUIRES} GENERATORS
-  cmake_find_package
+    REQUIRES lyra/1.6.0 BUILD_REQUIRES ${BUILD_REQUIRES} GENERATORS cmake_find_package
 )
 
 conan_cmake_autodetect(settings)
 
 conan_cmake_install(
-  PATH_OR_REFERENCE
-  .
-  BUILD
-  missing
-  REMOTE
-  conancenter
-  SETTINGS
-  ${settings}
+    PATH_OR_REFERENCE
+    .
+    BUILD
+    missing
+    REMOTE
+    conancenter
+    SETTINGS
+    ${settings}
 )
 
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR})
@@ -42,5 +36,5 @@ list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
 find_package(lyra)
 
 if(BUILD_TESTING)
-  find_package(doctest)
+    find_package(doctest)
 endif()
--- src/cmake/functions.cmake
+++ src/cmake/functions.cmake
@@ -1,17 +1,11 @@
 # convenience function for creating tests
 function(add_pass_test)
-  set(opts)
-  set(oneValueArgs BIN_NAME)
-  set(multiValueArgs BIN_SOURCES)
-  cmake_parse_arguments(
-    ADD_PASS_TEST "${opts}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN}
-  )
-  add_executable(${ADD_PASS_TEST_BIN_NAME} ${ADD_PASS_TEST_BIN_SOURCES})
-  target_include_directories(
-    ${ADD_PASS_TEST_BIN_NAME} PRIVATE ${CMAKE_CURRENT_LIST_DIR}
-  )
-  set(doctestArgs "-fc")
-  add_test(NAME ${ADD_PASS_TEST_BIN_NAME} COMMAND ${ADD_PASS_TEST_BIN_NAME}
-                                                  ${doctestArgs}
-  )
+    set(opts)
+    set(oneValueArgs BIN_NAME)
+    set(multiValueArgs BIN_SOURCES)
+    cmake_parse_arguments(ADD_PASS_TEST "${opts}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    add_executable(${ADD_PASS_TEST_BIN_NAME} ${ADD_PASS_TEST_BIN_SOURCES})
+    target_include_directories(${ADD_PASS_TEST_BIN_NAME} PRIVATE ${CMAKE_CURRENT_LIST_DIR})
+    set(doctestArgs "-fc")
+    add_test(NAME ${ADD_PASS_TEST_BIN_NAME} COMMAND ${ADD_PASS_TEST_BIN_NAME} ${doctestArgs})
 endfunction()
