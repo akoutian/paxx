5c92198aa38e6b48b1dc302ac974939fbcb0a4f1
Anastasios Koutian <akoutian2@gmail.com>
2023-08-03 22:13:00 +0300

    Add "show" help text

--- src/commands/info/src/info.cxx
+++ src/commands/info/src/info.cxx
@@ -23,6 +23,10 @@ void Help([[maybe_unused]] cmn::Context &ctx)
 
     std::cout << "\n";
     std::cout << "Usage:\n";
+    std::cout << "    pass show [--clip, -c] [--qrcode, -q] [--line=line-number, --line "
+                 "line-number, -l=line-number, -l line-number, -lline-number] pass-name\n";
+    std::cout << "        Show existing password and optionally put it on the clipboard or print "
+                 "it in the console as a QR code.\n";
     std::cout << "    pass rm [--recursive,-r] [--force,-f] pass-name\n";
     std::cout << "        Remove existing password or directory, optionally forcefully.\n";
     std::cout << "    pass help\n";
