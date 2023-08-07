6e0d4abfd28873c49441cbd543ecc4470ff28217
Anastasios Koutian <akoutian2@gmail.com>
2023-08-07 14:29:16 +0100

    Add dependency on xvmc
    It is needed by dacap-clip, however it looks like the conan recipe fails
    to pull this library in.

--- src/cmake/system-dependencies.cmake
+++ src/cmake/system-dependencies.cmake
@@ -1,1 +1,5 @@
 find_package(Gpgmepp REQUIRED)
+
+# transitive requirement of dacap-clip unfortuanely its recipe as of Thu Aug  3 11:33:53 PM EEST
+# 2023 does not require it, as it should
+find_library(xvmc XvMC REQUIRED)
